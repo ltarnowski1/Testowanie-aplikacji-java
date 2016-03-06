8ffaa91ea01daf35c481abee5036a6ce7cc15952
Łukasz Tarnowski <ukasz Tarnowski>
2016-03-06 22:09:23 +0100

    Suite

---
+++ Zad2Projekt1/Zad2Projekt1/src/test/java/maven/Zad2Projekt1/PsikusTestSuite.java
@@ -0,0 +1,18 @@
+package maven.Zad2Projekt1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  CyfroKradTest.class,
+  HeheszkiTest.class,
+  HultajChochlaTest.class,
+  NieksztaltekTest.class,
+  TititTest.class
+})
+
+public class PsikusTestSuite {
+
+}
+
